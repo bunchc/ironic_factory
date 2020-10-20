4551f6d1b0c4c9bbf14c16d6ec48aa62d3e8e5a4
Cody Bunch <bunchc@gmail.com>
2020-10-20 14:15:46 +0000

    action and smaller docker image

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -2,6 +2,6 @@
 
 starttime=$(date)
 echo "::set-output name=starttime::$starttime"
-/usr/bin/ansible-playbook $1 /playbooks/
+/usr/bin/ansible-playbook $1
 endtime=$(date)
 echo "::set-output name=endtime::$endtime"
