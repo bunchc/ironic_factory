b600df5fe118fdd36c671a740d7a4602dfb2765b
Cody Bunch <bunchc@gmail.com>
2020-10-23 16:59:06 +0000

    updates for ansible playbooks, multistage docker builds and parameters on the gh-action

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -2,6 +2,13 @@
 
 starttime=$(date)
 echo "::set-output name=starttime::$starttime"
-/usr/bin/ansible-playbook $1
+/usr/bin/ansible-playbook --version
+
+if [ -z "$2" ]; then
+  /usr/bin/ansible-playbook $1
+else
+  /usr/bin/ansible-playbook -i $2 $1
+fi
+
 endtime=$(date)
 echo "::set-output name=endtime::$endtime"
