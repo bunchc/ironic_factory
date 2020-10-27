f01afeafab6d5a1e3ddacf786889a99232983c67
Cody Bunch <bunchc@gmail.com>
2020-10-27 13:36:55 +0000

    feat(actions): add verbosity parameter

--- Docker/entrypoint.sh
+++ Docker/entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/sh -l
 
+# Parameters
+#
+# $1 - verbosity '-vvv'
+# $2 - playbook '/ansible/destroy.yml'
+# $3 - inventory
+
 starttime=$(date)
 echo "::set-output name=starttime::$starttime"
 /usr/bin/ansible-playbook --version
 
-if [ -z "$2" ]; then
-  /usr/bin/ansible-playbook $1
-else
-  /usr/bin/ansible-playbook -i $2 $1
+if [ -z "$3" ]; then # If no inventory file is passed, just run the playbook
+  /usr/bin/ansible-playbook $1 $2
+else # Otherwise pass the inventory
+  /usr/bin/ansible-playbook -i $3 $1 $2
 fi
 
 endtime=$(date)
